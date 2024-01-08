c5f9c504e73b3ebd67c843f3aa64994b8a2b6727
Crispus Makokha Omollo <126664434+crispusomollo@users.noreply.github.com>
2024-01-08 23:22:58 +0300

    Update states.py

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-'''Contains the states view for the API.'''
+'''Contains the states views for the API.'''
 from flask import jsonify, request
 from werkzeug.exceptions import NotFound, MethodNotAllowed, BadRequest
 
